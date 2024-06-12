76d5d312ea48a53bf25c242103d8fbb486ded3a5
million-art <millionmulugeta09@gmail.com>
2024-06-12 11:55:07 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Translation from './Translation'
+import Navbar from "./components/Navbar"
+function App() {
+ 
+  return (
+    <>
+      <Navbar/>
+      <Translation />
+    </>
+  )
+}
+
+export default App
